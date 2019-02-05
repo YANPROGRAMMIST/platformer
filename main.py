fc25ef7202dcc4ade4a9089f1262e59a4f1b30c5
Yan Apanovich <YAN.PROGRAMMIST@gmail.com>
2019-02-05 10:31:30 +0300

    lesson-16

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
 from player import Player
+
 pygame.init()
 screen = pygame.display.set_mode((WIDHT, HEIGHT)) 
 pygame.display.set_caption(World)
 clock = pygame.time.Clock()
 
 running = True
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 while running:
     clock.tick(FPS)
@@ -15,9 +20,11 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     
+    all_sprites.update()
     
 
     screen.fill(RED)
+    all_sprites.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 import pygame
 import os
-
+from settings import player_image
+from constants import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite,__init__(self)
-        
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(player_image).convert()
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDHT/2,HEIGHT/2)
