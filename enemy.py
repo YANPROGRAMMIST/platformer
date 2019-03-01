5b01e2e3e83d01ef895abfc8d1c1a7054c10e987
Ilya Labacheuski <ilya_labacheuski@epam.com>
2019-03-01 10:37:35 +0300

    lesson-20

--- enemy.py
+++ enemy.py
@@ -1,12 +1,13 @@
 import pygame
 from settings import enemy_image
 from constants import *
+from random import randint
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
         raw_surface = pygame.image.load(enemy_image).convert_alpha()
+
         scale_coefficient = ENEMY_SHIP_WIDTH / raw_surface.get_rect().width
         self.image =pygame.transform.rotozoom(
             raw_surface,\
@@ -15,4 +16,9 @@ class Enemy(pygame.sprite.Sprite):
         )
 
         self.rect = self.image.get_rect()
-        self.rect.midtop = (WIDTH / 2, 20)
+        Misha=self.rect.width/2
+        x = randint(0 + Misha, WIDTH + Misha)  
+        self.rect.midtop = (x, 20)
+        
+        
+
